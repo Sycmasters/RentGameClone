d33fb1380e907ccb8c39dff900d2e6d797242684
JSoto <josuesyc_1393@hotmail.com>
2017-08-25 00:21:42 -0600

    All connections from user registration/login, to rooms creations

--- Assets/Game/Scripts/Networking/LobbyHandler.cs
+++ Assets/Game/Scripts/Networking/LobbyHandler.cs
@@ -1,16 +1,205 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using PlayFab;
+using PlayFab.ClientModels;
 
-public class LobbyHandler : MonoBehaviour {
+public class LobbyHandler : MonoBehaviour
+{
+    [Header("Settings")]
+    public bool autoLogin = true;
 
-	// Use this for initialization
-	void Start () {
-		
+    [Header("Panels")]
+    public GameObject logRegisterPanel;
+    public GameObject loginPanel;
+    public GameObject registerPanel;
+    public GameObject lobbyPanel;
+    public GameObject roomPanel;
+    public GameObject roomNamePanel;
+
+    public Text errorMessage;
+
+    [Header("Login")]
+    public InputField loginUsername;
+    public InputField loginPassword;
+    public Button loginToRegisterBtn;
+    public Button loginBtn;
+    public Toggle rememberAcc;
+
+    [Header("Register")]
+    public InputField registerUsername;
+    public InputField registerPassword;
+    public InputField registerNickname;
+    public Button registerToLoginBtn;
+    public Button registerBtn;
+
+
+    [Header("NetworkInfo")]
+    private string titleID = "15A3";
+    private string photonId = "bfe5901d-b5ca-4959-9487-689e0ab08b76";
+
+    private void Awake()
+    {
+        // Are we already connected?
+        if(PlayerPersistentData.alreadyConnected)
+        {
+            logRegisterPanel.SetActive(false);
+            roomPanel.SetActive(true);
+        }
+    } 
+
+    private void Start()
+    {
+        if(PlayerPrefs.HasKey("RememberAccount"))
+        {
+            rememberAcc.isOn = PlayerPrefs.GetInt("RememberAccount") == 1 ? true : false;
+            loginUsername.text = PlayerPrefs.GetString("LoginUsername");
+            loginPassword.text = PlayerPrefs.GetString("LoginPassword");
+
+            if(autoLogin)
+            {
+                Login();
+            }
+        }
+    }
+
+    public void Login ()
+    {
+        // Clean 
+        errorMessage.text = "";
+
+        if (string.IsNullOrEmpty(loginUsername.text))
+        {
+            errorMessage.text = "Se requiere un nombre de usuario obligatorio.";
+            return;
+        }
+        else if (string.IsNullOrEmpty(loginPassword.text) || loginPassword.text.Length < 6)
+        {
+            errorMessage.text = "Se requiere una contraseña de minimo 6 letras.";
+            return;
+        }
+
+        // Disable login buttons
+        loginToRegisterBtn.interactable = false;
+        loginBtn.interactable = false;
+
+        if(rememberAcc.isOn)
+        {
+            PlayerPrefs.SetInt("RememberAccount", 1);
+            PlayerPrefs.SetString("LoginUsername", loginUsername.text);
+            PlayerPrefs.SetString("LoginPassword", loginPassword.text);
+        }
+
+        // Login with playfab
+        PlayFabClientAPI.LoginWithPlayFab(new LoginWithPlayFabRequest
+        {
+            TitleId = titleID,
+            Username = loginUsername.text,
+            Password = loginPassword.text
+        }, LoginCallback, PlayfabErrorCallback, null);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void LoginCallback (LoginResult result)
+    {
+        // Get needed data
+        PlayerPersistentData.playerID = result.PlayFabId;
+
+        // Get photon token
+        PlayFabClientAPI.GetPhotonAuthenticationToken(new GetPhotonAuthenticationTokenRequest
+        {
+            PhotonApplicationId = photonId
+        }, GetTokenCallback, PlayfabErrorCallback, null);
+    }
+
+    private void GetTokenCallback (GetPhotonAuthenticationTokenResult result)
+    {
+        // Clean 
+        errorMessage.text = "";
+
+        string photonToken = result.PhotonCustomAuthenticationToken;
+        Debug.Log(string.Format("Yay, logged in in session token: {0}", photonToken));
+        PhotonNetwork.AuthValues = new AuthenticationValues();
+        PhotonNetwork.AuthValues.AuthType = CustomAuthenticationType.Custom;
+        PhotonNetwork.AuthValues.AddAuthParameter("username", PlayerPersistentData.playerID);
+        PhotonNetwork.AuthValues.AddAuthParameter("Token", photonToken);
+        PhotonNetwork.ConnectToBestCloudServer("1.0");
+
+        PlayFabClientAPI.GetAccountInfo(new GetAccountInfoRequest { }, GetUserData, PlayfabErrorCallback, null);
+    }
+
+    private void GetUserData(GetAccountInfoResult result)
+    {
+        // Grab name
+        PlayerPersistentData.playerName = result.AccountInfo.TitleInfo.DisplayName;
+        PhotonNetwork.playerName = result.AccountInfo.TitleInfo.DisplayName;
+
+        // Open lobby
+        logRegisterPanel.SetActive(false);
+        lobbyPanel.SetActive(true);
+    }
+
+    public void RegisterNewUser ()
+    {
+        // Clean 
+        errorMessage.text = "";
+
+        if (string.IsNullOrEmpty(registerUsername.text))
+        {
+            errorMessage.text = "Se requiere un nombre de usuario obligatorio.";
+            return;
+        }
+        else if (string.IsNullOrEmpty(registerPassword.text) || registerPassword.text.Length < 6)
+        {
+            errorMessage.text = "Se requiere una contraseña de minimo 6 letras.";
+            return;
+        }
+        else if (string.IsNullOrEmpty(registerNickname.text))
+        {
+            errorMessage.text = "Por favor escriba el nombre con el que lo veran los demas jugadores.";
+            return;
+        }
+
+        // Disable register buttons
+        registerToLoginBtn.interactable = false;
+        registerBtn.interactable = false;
+
+        PlayFabClientAPI.RegisterPlayFabUser(new RegisterPlayFabUserRequest
+        {
+            Username = registerUsername.text,
+            Password = registerPassword.text,
+            DisplayName = registerNickname.text,
+            RequireBothUsernameAndEmail = false,
+            TitleId = titleID
+        }, RegisterCallback, PlayfabErrorCallback, null);
+    }
+
+    private void RegisterCallback (RegisterPlayFabUserResult result)
+    {
+        // Get needed data
+        PlayerPersistentData.playerID = result.PlayFabId;
+
+        // Get photon token
+        PlayFabClientAPI.GetPhotonAuthenticationToken(new GetPhotonAuthenticationTokenRequest
+        {
+            PhotonApplicationId = photonId
+        }, GetTokenCallback, PlayfabErrorCallback, null);
+    }
+
+    private void PlayfabErrorCallback (PlayFabError error)
+    {
+        Debug.Log(error.Error);
+        Debug.Log(error.ErrorMessage);
+
+        if(error.Error == PlayFabErrorCode.AccountNotFound)
+        {
+            errorMessage.text = "El usuario o el password son incorrectos";
+        }
+
+        loginToRegisterBtn.interactable = true;
+        loginBtn.interactable = true;
+
+        registerToLoginBtn.interactable = true;
+        registerBtn.interactable = true;
+    }
 }
---
+++ Assets/Game/Scripts/Networking/RoomHandler.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon;
+
+public class RoomHandler : Photon.MonoBehaviour
+{
+    [Header("Room Data")]
+    public GameObject[] playerInfo;
+    public List<int> roomPlayers;
+    public int playersInRoom = 0;
+
+    [Header("Required")]
+    public LobbyHandler lobby;
+    public InputField roomName;
+    public Text errorMessage;
+    public Button startGame;
+    public Button createRoomBtn;
+
+    public void CreateRoom ()
+    {
+        if(string.IsNullOrEmpty(roomName.text))
+        {
+            errorMessage.text = "Necesita un nombre para la sala obligatorio";
+            return;
+        }
+
+        createRoomBtn.interactable = false;
+
+        RoomOptions settings = new RoomOptions()
+        {
+            MaxPlayers = 5,
+            PlayerTtl = 300000,
+            EmptyRoomTtl = 0
+        };
+
+        PhotonNetwork.CreateRoom(roomName.text, settings, TypedLobby.Default);
+    }
+
+    private void OnJoinedRoom ()
+    {
+        // Show room wait
+        lobby.roomNamePanel.SetActive(false);
+        lobby.roomPanel.SetActive(true);
+
+        // We entered the room we just created
+        playersInRoom = PhotonNetwork.room.PlayerCount;
+        playerInfo[playersInRoom-1].SetActive(true);
+
+        // Set nick for everyone
+        playerInfo[playersInRoom - 1].transform.Find("Name").GetComponent<Text>().text = PhotonNetwork.playerName;
+        playerInfo[playersInRoom - 1].transform.Find("RightBtn").GetComponent<Button>().interactable = true;
+        playerInfo[playersInRoom - 1].transform.Find("LeftBtn").GetComponent<Button>().interactable = true;
+
+        roomPlayers.Add(PhotonNetwork.player.ID);
+
+        if (playersInRoom > 2)
+        {
+            startGame.interactable = true;
+        }
+    }
+
+    private void OnPhotonCreateRoomFailed (object[] codeAndMsg)
+    {
+        Debug.LogError("Create room failed: " + codeAndMsg[0]);
+        createRoomBtn.interactable = true;
+    }
+
+    private void OnPhotonPlayerConnected (PhotonPlayer otherPlayer)
+    {
+        // We entered the room we just created
+        playersInRoom = PhotonNetwork.room.PlayerCount;
+        playerInfo[playersInRoom - 1].SetActive(true);
+
+        // Set nick for everyone and buttons for avatar
+        playerInfo[playersInRoom - 1].transform.Find("Name").GetComponent<Text>().text = otherPlayer.NickName;
+
+        roomPlayers.Add(otherPlayer.ID);
+
+        if (playersInRoom > 2)
+        {
+            startGame.interactable = true;
+        }
+    }
+
+    private void OnPhotonPlayerDisconnected(PhotonPlayer otherPlayer)
+    {
+        int index = roomPlayers.IndexOf(otherPlayer.ID);
+
+        playerInfo[index].SetActive(false);
+        playersInRoom = PhotonNetwork.room.PlayerCount;
+
+        // Set nick for everyone
+        playerInfo[index].transform.Find("Name").GetComponent<Text>().text = "Buscando jugador";
+    }
+}
